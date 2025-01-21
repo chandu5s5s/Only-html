86191e51bfa0b09d5b1f4f7e70983fed06b3d852
chandu5s5s <replitcoresuryapaid@gmail.com>
2025-01-22 01:52:35 +0530

    Create a.js

---
+++ a.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+const port = process.env.PORT || 15000;
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route to display HTML file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
