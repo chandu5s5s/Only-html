5c3014eea2b48b43992daf336e2751f49835585f
chandu5s5s <replitcoresuryapaid@gmail.com>
2025-01-22 02:04:33 +0530

    Update a.js

--- a.js
+++ a.js
@@ -1,17 +1,15 @@
-import express from 'express';
-import path from 'path';
+const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 15000;
 
-// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Route to display HTML file
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server running on http://localhost:${port}`);
 });
